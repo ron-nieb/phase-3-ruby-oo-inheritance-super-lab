f79a887d98da38c3cfabf4f688e03b8a75073b09
Ronald Barasa <barasaogoro@gmail.com>
2023-05-24 00:54:50 +0300

    done

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,12 +1,11 @@
-class ChattyStudent
+class ChattyStudent < Student
     def hello
-      greeting = "Hey there! I'm so excited to learn stuff.\n"
-      chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-      puts greeting + chatty_phrase
+      super
+      chatty_phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well, let me just tell you who died..."
+      puts chatty_phrase
     end
-  
+    
     def raise_hand
-      phrase = "Pick me!\n"
-      10.times { puts phrase }
+      10.times { super }
     end
   end
